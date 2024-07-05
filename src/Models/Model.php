845cc6cadcc6bf9166df5b5ae3113ee7a11eaad8
Brice1827 <61006271+Brice1827@users.noreply.github.com>
2024-07-05 17:52:37 +0200

    Update Model.php
    new function createMultiplePkNotId for tables where pk is not 'id'

--- src/Models/Model.php
+++ src/Models/Model.php
@@ -42,4 +42,40 @@ class Model extends \yii\base\Model
 
         return $models;
     }
+
+    /**
+     * Creates and populates a set of models when table primary key is not "id".
+     *
+     * @param string $modelClass
+     * @param array $multipleModels
+     * @param string $pk
+     * @return array
+     */
+    public static function createMultiplePkNotId($modelClass, $multipleModels = [], $pk = 'id')
+    {
+        $model    = new $modelClass;
+        $formName = $model->formName();
+        $post     = Yii::$app->request->post($formName);
+        $models   = [];
+
+        if (!empty($multipleModels)) {
+            $keys = array_keys(ArrayHelper::map($multipleModels, $pk, $pk));
+            $multipleModels = array_combine($keys, $multipleModels);
+        }
+
+        if ($post && is_array($post)) {
+            foreach ($post as $i => $item) {
+                if (isset($item['id']) && !empty($item[$pk]) && isset($multipleModels[$item[$pk]])) {
+                    $models[] = $multipleModels[$item[$pk]];
+                } else {
+                    $models[] = new $modelClass;
+                }
+            }
+        }
+
+        unset($model, $formName, $post);
+
+        return $models;
+    }
+
 }
